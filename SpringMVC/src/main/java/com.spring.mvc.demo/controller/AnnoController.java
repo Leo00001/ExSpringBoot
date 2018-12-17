176ce02c098c4d77c0405394ae7ef23f61c8feba
baiyuas <liup00001@163.com>
2018-12-18 00:04:34 +0800

    Add:增加两个注解使用

--- SpringMVC/src/main/java/com.spring.mvc.demo/controller/AnnoController.java
+++ SpringMVC/src/main/java/com.spring.mvc.demo/controller/AnnoController.java
@@ -1,6 +1,11 @@
 package com.spring.mvc.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author baiyu
@@ -8,10 +13,33 @@ import org.springframework.stereotype.Controller;
  * MVC基本注解使用
  */
 @Controller
+@RequestMapping("anno")
 public class AnnoController {
 
 
+    /**
+     * /@RequestBody返回对象
+     *
+     * @param request HttpServletRequest
+     * @return 返回字符串
+     */
+    @RequestMapping(produces = "text/plain;charset=utf8")
+    @ResponseBody
+    public String index(HttpServletRequest request) {
 
+        return "url:" + request.getRequestURI() + " can access";
+    }
 
+    /**
+     * /@PathVariable 路径变量，使用与get请求url中
+     *
+     * @param name 路径参数name
+     * @return 返回字符串
+     */
+    @RequestMapping(value = "/path_var/{name}", produces = "text/plain;charset=utf8")
+    @ResponseBody
+    public String demoPathVar(@PathVariable String name) {
+        return "Welcome " + name + " visit our site";
+    }
 
 }
